bd2ea5f63f99eb843fe771c26f4cf0190e250763
Raul Semicek Coelho <raul.s.coelho@gmail.com>
2022-04-16 05:18:05 -0300

    backend seted

--- src/index.js
+++ src/index.js
@@ -13,12 +13,11 @@ mongoose.connect(process.env.DB_CONNECT,
     {
         useNewUrlParser: true,
         useUnifiedTopology: true,
-    },
-    () => console.log('connected to DB http://localhost:3001/')
+    }
 )
 
 //Middlewares
-app.use(cors({ origin: 'http://localhost:3000', credentials: true }))
+app.use(cors({ origin: 'https://lifeofadream.vercel.app', credentials: true }))
 app.use(express.json())
 app.use(cookieParser())
 
